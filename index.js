e7578655ba08d648c59336f24a7844c299df9dc7
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-03-25 17:48:02 -0400

    changed home screen

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello qoe")
+
+function changeNavColor() {
+
+    let navBar = document.querySelector('#navBar')
+    let prevScrollPos = 0
+
+    window.addEventListener('scroll', () => {
+
+        if((document.body.getBoundingClientRect()).top > prevScrollPos){
+            navBar.id = "navBarScrolled"
+            console.log("up")
+        }else if(document.body.getBoundingClientRect().top === 0){
+            navBar.id = "navBar"
+        }
+        else{
+            console.log("sown")
+            navBar.id = "navBar"
+        }
+        prevScrollPos = (document.body.getBoundingClientRect()).top;
+    })
+}
+
+    changeNavColor()
