3785ee736d93de55e31b7fd7387028b9e32f43ba
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-04-14 15:48:28 -0400

    finished

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    fetch('http://localhost:3000/visiteds')
+    
+    let objectConfig = {
+        method:"POST",
+        headers: {
+    
+            'Content-Type': 'application/json',
+            "Accept": 'application/json'
+    
+        },
+        body: JSON.stringify({
+            
+           times: 1
+    
+        })
+    }
+
+    fetch('http://localhost:3000/visiteds', objectConfig)
     .then(res => res.json())
-    .then(visited => console.log(visited))
-    console.log("im here")
+    .then(res => console.log(res))
+   
+    console.log(window.location.origin )
+
 })
+
+
